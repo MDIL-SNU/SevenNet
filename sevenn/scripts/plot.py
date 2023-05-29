9ccde1b334ae695ebe753e66b8ffe4fc7c6d9913
YutackPark <parkyutack@DESKTOP-0A0T0NC>
2023-05-29 14:52:01 +0900

    plot lc fix

--- sevenn/scripts/plot.py
+++ sevenn/scripts/plot.py
@@ -7,23 +7,6 @@ matplotlib.use('pdf')
 
 
 def draw_learning_curve(loss_history, fname):
-    """
-    try:
-        draw_learning_curve.history[DataSetType.TRAIN][LossType.ENERGY].append(
-            train_loss['total'][LossType.ENERGY])
-        draw_learning_curve.history[DataSetType.TRAIN][LossType.FORCE].append(
-            train_loss['total'][LossType.FORCE])
-        draw_learning_curve.history[DataSetType.VALID][LossType.ENERGY].append(
-            valid_loss['total'][LossType.ENERGY])
-        draw_learning_curve.history[DataSetType.VALID][LossType.FORCE].append(
-            valid_loss['total'][LossType.FORCE])
-    except AttributeError:
-        draw_learning_curve.history = {
-            DataSetType.TRAIN: {LossType.ENERGY: [], LossType.FORCE: []},
-            DataSetType.VALID: {LossType.ENERGY: [], LossType.FORCE: []}
-        }
-    """
-
     # alias
     train_hist = loss_history[DataSetType.TRAIN]
     valid_hist = loss_history[DataSetType.VALID]
@@ -55,45 +38,7 @@ def draw_learning_curve(loss_history, fname):
     force_ax.legend()
 
     plt.savefig(fname, format="png")
-
-
-def draw_learning_curve_deprecated(loss_hist, fname):
-    # do nothinig untill 10 epoch
-    if len(loss_hist[DataSetType.TRAIN]['total']['energy']) < 10:
-        return
-    plt.clf()
-
-    # cut unusually large loss at early epoch for visual
-
-    # TODO: refactor
-    loss_hist[DataSetType.TRAIN]['total']['energy'] = \
-        loss_hist[DataSetType.TRAIN]['total']['energy'][5:]
-    loss_hist[DataSetType.TRAIN]['total']['force'] = \
-        loss_hist[DataSetType.TRAIN]['total']['force'][5:]
-    loss_hist[DataSetType.VALID]['total']['energy'] = \
-        loss_hist[DataSetType.VALID]['total']['energy'][5:]
-    loss_hist[DataSetType.VALID]['total']['force'] = \
-        loss_hist[DataSetType.VALID]['total']['force'][5:]
-
-    fig, axs = plt.subplots(2, 1, figsize=(9, 6), dpi=300, constrained_layout=True)
-
-    energy_ax = axs[0]
-    force_ax = axs[1]
-    energy_ax.plot(loss_hist[DataSetType.TRAIN]['total']['energy'], label='train')
-    energy_ax.plot(loss_hist[DataSetType.VALID]['total']['energy'], label='valid')
-    energy_ax.set_xlabel('Epoch')
-    energy_ax.set_ylabel('RMSE(eV/atom)')
-    energy_ax.set_title('Energy')
-    energy_ax.legend()
-
-    force_ax.plot(loss_hist[DataSetType.TRAIN]['total']['force'], label='train')
-    force_ax.plot(loss_hist[DataSetType.VALID]['total']['force'], label='valid')
-    force_ax.set_xlabel('Epoch')
-    force_ax.set_ylabel(r'RMSE(eV/${\rm \AA}$)')
-    force_ax.set_title('Force')
-    force_ax.legend()
-
-    plt.savefig(fname, format="png")
+    plt.close()
 
 
 # TODO: implement
