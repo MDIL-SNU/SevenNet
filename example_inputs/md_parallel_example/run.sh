adc6e5a35a85fa6c5037e7332c7eab423b9b0cbd
YutackPark <parkyutack@DESKTOP-0A0T0NC>
2023-06-29 12:56:10 +0900

    remove run.sh

--- example_inputs/md_parallel_example/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export CUDA_VISIBLE_DEVICES=
-mpirun -np 2 /TGM/Apps/LAMMPS/lammps/build_testing/lmp < ./in.lmp
-
-
--- example_inputs/md_serial_example/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#export CUDA_VISIBLE_DEVICES=
-/TGM/Apps/LAMMPS/lammps/build_testing/lmp < ./in.lmp
-
-
