6b174a3546b52e9513dafbe6e0689e8639553004
YutackPark <parkyutack@DESKTOP-0A0T0NC>
2023-08-14 14:01:41 +0900

    It fixes error about stress

--- sevenn/nn/edge_embedding.py
+++ sevenn/nn/edge_embedding.py
@@ -47,8 +47,10 @@ class EdgePreprocess(nn.Module):
                 data["_strain"] = strain
 
                 sym_strain = 0.5 * (strain + strain.transpose(-1, -2))
-                pos += torch.bmm(pos.unsqueeze(-2), sym_strain[batch]).squeeze(-2)
-                cell += torch.bmm(cell, sym_strain)
+                # Do not modify it to pos += or cell += !!!!!
+                pos = pos + \
+                    torch.bmm(pos.unsqueeze(-2), sym_strain[batch]).squeeze(-2)
+                cell = cell + torch.bmm(cell, sym_strain)
 
         idx_src = data[KEY.EDGE_IDX][0]
         idx_dst = data[KEY.EDGE_IDX][1]
