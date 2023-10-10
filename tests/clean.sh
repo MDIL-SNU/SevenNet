52644e63249871030c33b7c66ee01b1c6a43e650
YutackPark <parkyutack@DESKTOP-0A0T0NC>
2023-10-10 19:08:24 +0900

    Automated end2end test bot online!

---
+++ tests/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf ./e3gnn_test
+rm -rf ./e3gnn_333
+rm -rf ./e3gnn_parallel_333
+rm -rf ./inference
+rm -rf ./potential
+rm -rf ./training
+
+rm ./graph_build_log
+rm ./data.sevenn_data
+
+
