70c40c60b1a07aa5571133593d7cc3ee63d37605
YutackPark <parkyutack@snu.ac.kr>
2024-09-18 17:18:55 +0900

    refactor: correct logger usage, graph build script

--- sevenn/scripts/graph_build.py
+++ sevenn/scripts/graph_build.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from sevenn.sevenn_logger import Logger
 from sevenn.train.dataload import file_to_dataset, match_reader
@@ -28,12 +29,13 @@ def dataset_finalize(
     elif out.endswith('.sevenn_data') is False:
         out = out + '.sevenn_data'
 
+    logger = Logger()
     if verbose:
-        Logger().writeline('The metadata of the dataset is...')
+        logger.writeline('The metadata of the dataset is...')
         for k, v in metadata.items():
-            Logger().format_k_v(k, v, write=True)
+            logger.format_k_v(k, v, write=True)
     dataset.save(out, save_by_label)
-    Logger().writeline(f'dataset is saved to {out}')
+    logger.writeline(f'dataset is saved to {out}')
 
     return dataset
 
@@ -48,40 +50,42 @@ def build_script(
     fmt: str,
     suffix: str,
     transfer_info: bool,
-    metadata: dict = None,
-    fmt_kwargs: dict = None,
+    metadata: Optional[dict] = None,
+    **fmt_kwargs,
 ):
-
+    if metadata is None:
+        metadata = {}
     reader, rmeta = match_reader(fmt, **fmt_kwargs)
     metadata.update(**rmeta)
     dataset = AtomGraphDataset({}, cutoff)
 
+    logger = Logger()
     if os.path.isdir(source):
-        Logger().writeline(f'Look for source dir: {source}')
+        logger.writeline(f'Look for source dir: {source}')
         if suffix is not None:
-            Logger().writeline(f'Try to read files if it ends with {suffix}')
+            logger.writeline(f'Try to read files if it ends with {suffix}')
         for file in os.listdir(source):
             label = file.split('.')[0] if label_by == 'auto' else label_by
             file = os.path.join(source, file)
             if suffix is not None and file.endswith(suffix) is False:
                 continue
-            Logger().writeline(f'Read from file: {file}')
-            Logger().timer_start('graph_build')
+            logger.writeline(f'Read from file: {file}')
+            logger.timer_start('graph_build')
             db = file_to_dataset(
                 file, cutoff, num_cores, reader, label, transfer_info
             )
             dataset.augment(db)
-            Logger().timer_end('graph_build', f'{label} graph build time')
+            logger.timer_end('graph_build', f'{label} graph build time')
     elif os.path.isfile(source):
         file = source
         label = file.split('.')[0] if label_by == 'auto' else label_by
-        Logger().writeline(f'Read from file: {file}')
-        Logger().timer_start('graph_build')
+        logger.writeline(f'Read from file: {file}')
+        logger.timer_start('graph_build')
         db = file_to_dataset(
             file, cutoff, num_cores, reader, label, transfer_info
         )
         dataset.augment(db)
-        Logger().timer_end('graph_build', f'{label} graph build time')
+        logger.timer_end('graph_build', f'{label} graph build time')
     else:
         raise ValueError(f'source {source} is not a file or dir')
 
