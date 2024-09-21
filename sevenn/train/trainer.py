296f5cd076411e8bd52ac003d9f93a74c7d21ed9
YutackPark <parkyutack@snu.ac.kr>
2024-09-21 21:31:58 +0900

    refactor: remove trainer config dependency

--- sevenn/train/trainer.py
+++ sevenn/train/trainer.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Iterable, Optional
+import os
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 
 import torch
 import torch.distributed as dist
@@ -7,39 +8,111 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 import sevenn._keys as KEY
 from sevenn.error_recorder import ErrorRecorder
+from sevenn.train.loss import LossDefinition
 
 from .loss import get_loss_functions_from_config
 from .optim import optim_dict, scheduler_dict
 
 
 class Trainer:
-    def __init__(self, model: torch.nn.Module, config: Dict[str, Any]):
-        self.distributed = config[KEY.IS_DDP]
+    """
+    Training routine specialized for this package
+    """
 
-        if self.distributed:
-            device = torch.device('cuda', config[KEY.LOCAL_RANK])
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        loss_functions: List[Tuple[LossDefinition, float]],
+        optimizer_cls,
+        optimizer_args: Optional[dict] = None,
+        scheduler_cls=None,
+        scheduler_args: Optional[dict] = None,
+        device: Union[torch.device, str] = 'auto',
+        distributed: bool = False,
+        distributed_backend: str = 'nccl',
+    ):
+        if device == 'auto':
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        if distributed:
+            local_rank = int(os.environ['LOCAL_RANK'])
+            self.rank = local_rank
+            if distributed_backend == 'nccl':
+                device = torch.device('cuda', local_rank)
+                self.model = DDP(model.to(device), device_ids=[device])
+            elif distributed_backend == 'mpi':
+                self.model = DDP(model.to(device))
+            else:
+                raise ValueError(f'Unknown DDP backend: {distributed_backend}')
             dist.barrier()
-            self.model = DDP(model.to(device), device_ids=[device])
             self.model.module.set_is_batch_data(True)
-            self.rank = config[KEY.LOCAL_RANK]
         else:
-            device = config[KEY.DEVICE]
             self.model = model.to(device)
             self.model.set_is_batch_data(True)
+            self.rank = 0
+
         self.device = device
+        self.distributed = distributed
 
         param = [p for p in self.model.parameters() if p.requires_grad]
-        optimizer = optim_dict[config[KEY.OPTIMIZER].lower()]
-        optim_param = config[KEY.OPTIM_PARAM]
-        self.optimizer = optimizer(param, **optim_param)
+        self.optimizer = optimizer_cls(param, **optimizer_args)
+        if scheduler_cls is not None:
+            self.scheduler = scheduler_cls(self.optimizer, **scheduler_args)
+        else:
+            self.scheduler = None
+        self.loss_functions = loss_functions
 
-        scheduler = scheduler_dict[config[KEY.SCHEDULER].lower()]
-        scheduler_param = config[KEY.SCHEDULER_PARAM]
-        self.scheduler = scheduler(self.optimizer, **scheduler_param)
+    @staticmethod
+    def from_config(model: torch.nn.Module, config: Dict[str, Any]):
+        trainer = Trainer(
+            model,
+            loss_functions=get_loss_functions_from_config(config),
+            optimizer_cls=optim_dict[config[KEY.OPTIMIZER].lower()],
+            optimizer_args=config[KEY.OPTIM_PARAM],
+            scheduler_cls=scheduler_dict[config[KEY.SCHEDULER].lower()],
+            scheduler_args=config[KEY.SCHEDULER_PARAM],
+            device=config[KEY.DEVICE],
+            distributed=config[KEY.IS_DDP],
+            distributed_backend='nccl',
+        )
+        return trainer
 
-        # This should be outside of the trainer(?)
-        # list of tuples (loss_definition, weight)
-        self.loss_functions = get_loss_functions_from_config(config)
+    @staticmethod
+    def args_from_checkpoint(checkpoint: str):
+        """
+        Usage:
+            trainer_args, optim_stct, scheduler_stct = args_from_checkpoint('7net-0')
+            # Do what you want to do here
+            trainer = Trainer(**trainer_args)
+            trainer.load_state_dict(
+                optimizer_state_dict=optim_stct,
+                scheduler_state_dict=scheduler_stct,
+        """
+        from sevenn.util import model_from_checkpoint, pretrained_name_to_path
+
+        if os.path.isfile(checkpoint):
+            checkpoint = checkpoint
+        else:
+            checkpoint = pretrained_name_to_path(checkpoint)
+
+        cp = torch.load(checkpoint)
+        model, config = model_from_checkpoint(cp)
+        optimizer_cls = optim_dict[config[KEY.OPTIMIZER].lower()]
+        scheduler_cls = scheduler_dict[config[KEY.SCHEDULER].lower()]
+        loss_functions = get_loss_functions_from_config(config)
+
+        return (
+            {
+                'model': model,
+                'loss_functions': loss_functions,
+                'optimizer_cls': optimizer_cls,
+                'optimizer_args': config[KEY.OPTIM_PARAM],
+                'scheduler_cls': scheduler_cls,
+                'scheduler_args': config[KEY.SCHEDULER_PARAM],
+            },
+            cp['optimizer_state_dict'],
+            cp['scheduler_state_dict'],
+        )
 
     def run_one_epoch(
         self,
@@ -72,9 +145,7 @@ class Trainer:
     def scheduler_step(self, metric: Optional[float] = None):
         if self.scheduler is None:
             return
-        if isinstance(
-            self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau
-        ):
+        if isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
             assert isinstance(metric, float)
             self.scheduler.step(metric)
         else:
@@ -96,7 +167,9 @@ class Trainer:
         return {
             'model_state_dict': model_state_dct,
             'optimizer_state_dict': self.optimizer.state_dict(),
-            'scheduler_state_dict': self.scheduler.state_dict(),
+            'scheduler_state_dict': self.scheduler.state_dict()
+            if self.scheduler is not None
+            else None,
         }
 
     def write_checkpoint(self, path: str, **extra):
@@ -108,17 +181,18 @@ class Trainer:
 
     def load_state_dicts(
         self,
-        model_state_dict: Dict,
-        optimizer_state_dict: Dict,
-        scheduler_state_dict: Dict,
+        model_state_dict: Optional[Dict] = None,
+        optimizer_state_dict: Optional[Dict] = None,
+        scheduler_state_dict: Optional[Dict] = None,
         strict: bool = True,
     ):
-        if self.distributed:
-            self.model.module.load_state_dict(model_state_dict, strict=strict)
-        else:
-            self.model.load_state_dict(model_state_dict, strict=strict)
+        if model_state_dict is not None:
+            if self.distributed:
+                self.model.module.load_state_dict(model_state_dict, strict=strict)
+            else:
+                self.model.load_state_dict(model_state_dict, strict=strict)
 
         if optimizer_state_dict is not None:
             self.optimizer.load_state_dict(optimizer_state_dict)
-        if scheduler_state_dict is not None:
+        if scheduler_state_dict is not None and self.scheduler is not None:
             self.scheduler.load_state_dict(scheduler_state_dict)
