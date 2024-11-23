fa7d07751aab6dc40e788d521b7f975fccda2ac7
YutackPark <parkyutack@snu.ac.kr>
2024-11-23 17:29:26 +0900

    fix: save real first epoch cp

--- sevenn/scripts/processing_epoch.py
+++ sevenn/scripts/processing_epoch.py
@@ -58,8 +58,9 @@ def processing_epoch_v2(
         with open(csv_path, 'w') as f:
             f.write(','.join(head) + '\n')
 
-    path = f'{prefix}/checkpoint_0.pth'  # save first epoch
-    trainer.write_checkpoint(path, config=config, epoch=0)
+    if start_epoch == 0:
+        path = f'{prefix}/checkpoint_0.pth'  # save first epoch
+        trainer.write_checkpoint(path, config=config, epoch=0)
 
     for epoch in range(start_epoch, total_epoch + 1):  # one indexing
         log.timer_start('epoch')
