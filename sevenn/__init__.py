53c99623ae20a13a0ff12077c34657fc9b9a484b
YutackPark <parkyutack@snu.ac.kr>
2025-04-29 13:30:24 +0900

    add strict e3nn version contraint

--- sevenn/__init__.py
+++ sevenn/__init__.py
@@ -1,3 +1,13 @@
 from importlib.metadata import version
 
+from packaging.version import Version
+
 __version__ = version('sevenn')
+
+from e3nn import __version__ as e3nn_ver
+
+if Version(e3nn_ver) < Version('0.5.0'):
+    raise ValueError(
+        'The e3nn version MUST be 0.5.0 or later due to changes in CG coefficient '
+        'convention.'
+    )
